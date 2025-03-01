4fd114369feffa844cd796166c132d9f76bb7ec9
luanmai <luandjhhdh@gmail.com>
2025-03-02 00:12:10 +0700

    Done Information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
+// import Image from "next/image";
+import Information from "@/layouts/Information/index"
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-4xl font-bold text-center text-blue-600 mt-8">
-        Ecommerce Library
-      </h1>
-    </>
+    <div className="main-container w-full h-auto">
+      <Information/>
+    </div>
   );
 }
---
+++ src/configs/images.ts
@@ -0,0 +1,16 @@
+import Group from '@/assets/imgs/Group.png'
+import Illustration from "@/assets/imgs/Illustration.png";
+import icon_park_email_block from "@/assets/imgs/icon-park_email-block.png";
+import logos_facebook from "@/assets/imgs/logos_facebook.png";
+import logos_twitter from "@/assets/imgs/logos_twitter.png";
+
+
+const images = {
+    Group,
+    Illustration,
+    icon_park_email_block,
+    logos_facebook,
+    logos_twitter
+};
+
+export default images;
